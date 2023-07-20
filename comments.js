f06fb32d217dca7de7df44f53e013a9c72684be3
Ahmed Murtaza <128592697+AhmeddMurtazaa@users.noreply.github.com>
2023-07-20 09:42:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web server
+// Import required modules
+const express = require('express');
+
+// Create an instance of the Express application
+const app = express();
+const port = 3000; // You can use any port you prefer
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
